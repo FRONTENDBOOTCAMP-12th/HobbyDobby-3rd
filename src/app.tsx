15175338fcb84e04f9aabd3a05f7db2243fc383c
kongsh <ksh080838@gmail.com>
2025-03-04 17:55:50 +0900

    [Modify] app.tsx
    - 개발 용으로 사용했던 LoginPage 컴포넌트 주석처리

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,11 @@
 import { ErrorBoundary } from 'react-error-boundary';
 import PrintError from '@/components/ErrorBoundary';
-import LoginPage from '@/pages/login';
+// import LoginPage from '@/pages/login';
 
 function App() {
   return (
     <ErrorBoundary FallbackComponent={PrintError}>
-      <LoginPage />
+      {/* <LoginPage /> */}
     </ErrorBoundary>
   );
 }
