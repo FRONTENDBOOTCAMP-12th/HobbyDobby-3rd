39ac53d6ac022937597f4a6f8fd12d4b092b2314
kongsh <ksh080838@gmail.com>
2025-03-04 17:53:36 +0900

    [Feat] 로그인 컴포넌트
    - 로그인 폼
    - 로그인 인풋

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -4,8 +4,18 @@ import LoginInput from './LoginInput';
 function LoginForm() {
   return (
     <form className="login-form">
-      <LoginInput type="email" label="아이디" />
-      <LoginInput type="password" label="비밀번호" />
+      <LoginInput
+        isLabelSrOnly={true}
+        type="email"
+        label="아이디"
+        placeholder="아이디를 입력해주세요."
+      />
+      <LoginInput
+        isLabelSrOnly={true}
+        type="password"
+        label="비밀번호"
+        placeholder="비밀번호를 입력해주세요."
+      />
       <button type="submit">로그인</button>
     </form>
   );
--- src/components/Login/LoginInput.tsx
+++ src/components/Login/LoginInput.tsx
@@ -1,13 +1,19 @@
-import './login.css';
-
 type LoginInputProps = React.ComponentProps<'input'> & {
   label: string;
+  isLabelSrOnly?: boolean;
 };
 
-function LoginInput({ id, label, ...restProps }: LoginInputProps) {
+function LoginInput({
+  id,
+  label,
+  isLabelSrOnly = false,
+  ...restProps
+}: LoginInputProps) {
+  const labelSrOnly = isLabelSrOnly ? 'sr-only' : '';
+
   return (
     <div className="login-input">
-      <label className="sr-only" htmlFor={id}>
+      <label className={labelSrOnly} htmlFor={id}>
         {label}
       </label>
       <input id={id} {...restProps} />
