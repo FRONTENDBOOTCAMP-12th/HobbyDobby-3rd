e871277f4fa49e5c9e83fdffff362d253bca497a
lyb9030 <lyb_9030@naver.com>
2025-03-05 17:39:00 +0900

    [Modify] : H1 추가 로고 alt 및 이미지 alt 변경 로고 및 이미지 header로 묶음

--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -3,23 +3,30 @@ import './style.css';
 function LandingPage() {
   return (
     <main>
-      <div>
-        {/* 로고 */}
+      <h1 className="sr-only">Hobby Dobby</h1>
+      <header>
         <div>
-          <img src="/assets/logo.svg" alt="Logo" className="logo" />
-          {/* 로고 텍스트 이미지 */}
-          <div className="logo-text">
-            <img src="/assets/hobby.svg" alt="Hobby" className="hobby-image" />
-            <img src="/assets/dobby.svg" alt="Dobby" className="dobby-image" />
+          {/* 로고 */}
+          <div>
+            <img
+              src="/assets/logo.svg"
+              alt="Hobby Dobby 로고"
+              className="logo"
+            />
+            {/* 로고 텍스트 이미지 */}
+            <div className="logo-text">
+              <img src="/assets/hobby.svg" alt="" className="hobby-image" />
+              <img src="/assets/dobby.svg" alt="" className="dobby-image" />
+            </div>
           </div>
         </div>
+      </header>
 
-        {/* 버튼 */}
-        <div>
-          <button type="button" className="start">
-            시작하기
-          </button>
-        </div>
+      {/* 버튼 */}
+      <div>
+        <button type="button" className="start">
+          시작하기
+        </button>
       </div>
     </main>
   );
