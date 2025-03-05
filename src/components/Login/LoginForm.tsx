bfc126a4942d0cb61d71b1414c78ea7cf5114b99
kongsh <ksh080838@gmail.com>
2025-03-05 15:45:34 +0900

    [Feat] 로그인 기능 추가
    - 로그인 필드 값의 유효성을 검사
    - 로그인 필드 값과 DB의 User 테이블과 대조해 로그인 기능 추가

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './login.css';
 import LoginInput from './LoginInput';
+import { getUserById } from '@/lib/api';
 
 interface FormInputData {
   id: string;
@@ -12,7 +13,7 @@ interface EventData {
   value: string;
 }
 
-interface Alert {
+interface AlertFlag {
   isValidId: boolean;
   isValidPw: boolean;
 }
@@ -21,26 +22,28 @@ const idRegex = /^(?=.*[a-zA-Z])[a-zA-Z0-9]{6,}$/;
 const pwRegex = /^[a-zA-Z0-9!@#$%^&*()-_+=]{8,}$/;
 
 function LoginForm() {
+  // 입력 데이터
   const [inputData, setInputData] = useState<FormInputData>({
     id: '',
     password: '',
   });
 
-  const [alert, setAlert] = useState<Alert>({
+  // 값에 대한 경고 플래그
+  const [alertFlag, setAlertFlag] = useState<AlertFlag>({
     isValidId: true,
     isValidPw: true,
   });
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.currentTarget as EventData;
-    const nextAlert = { ...alert };
+    const nextAlertFlag = { ...alertFlag };
 
     switch (name) {
       case 'id':
-        nextAlert.isValidId = idRegex.test(value) ? true : false;
+        nextAlertFlag.isValidId = idRegex.test(value) ? true : false;
         break;
       case 'password':
-        nextAlert.isValidPw = pwRegex.test(value) ? true : false;
+        nextAlertFlag.isValidPw = pwRegex.test(value) ? true : false;
     }
 
     const nextInputData = {
@@ -49,11 +52,47 @@ function LoginForm() {
     };
 
     setInputData(nextInputData);
-    setAlert(nextAlert);
+    setAlertFlag(nextAlertFlag);
+  };
+
+  const handleLogIn = async (formData: FormData) => {
+    const inputId = formData.get('id') as string;
+
+    try {
+      const { data: user, error } = await getUserById(inputId);
+
+      if (user) {
+        const nextInputData = { id: '', password: '' };
+
+        if (user?.length > 0) {
+          const inputPw = formData.get('password') as string;
+          const userData = user[0];
+
+          if (userData.password === inputPw) {
+            console.log('login!!');
+            // 로그인이 되었다는 토스트 / 알림
+            // 필요한 데이터 저장(zustand or local Storage or ...?) 페이지 이동
+          } else {
+            console.log('invalid PW');
+            // 비밀번호가 올바르지 않다는 토스트(라이브러리 사용?) / 알림
+            nextInputData.id = inputData.id;
+            setInputData(nextInputData);
+          }
+        } else {
+          console.log('invalid ID');
+          // 아이디가 올바르지 않다는 토스트(라이브러리 사용?) / 알림
+          setInputData(nextInputData);
+        }
+      } else {
+        throw error;
+      }
+    } catch (err) {
+      throw new Error(err as string);
+    }
   };
 
   return (
-    <form className="login-form">
+    <form className="login-form" action={handleLogIn}>
       <LoginInput
         isLabelSrOnly={true}
         type="text"
@@ -63,7 +102,7 @@ function LoginForm() {
         value={inputData.id}
         alertMessage="최소 6자가 필요합니다."
         onChange={handleInput}
-        isValidInput={alert.isValidId}
+        isValidInput={alertFlag.isValidId}
       />
       <LoginInput
         isLabelSrOnly={true}
@@ -74,7 +113,7 @@ function LoginForm() {
         value={inputData.password}
         alertMessage="최소 8자와 특수문자가 필요합니다."
         onChange={handleInput}
-        isValidInput={alert.isValidPw}
+        isValidInput={alertFlag.isValidPw}
       />
       <button type="submit">로그인</button>
     </form>
