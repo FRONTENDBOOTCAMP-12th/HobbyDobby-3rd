c83a4cd127abe924bfb617eda05ed16aed9fd0e7
Hyerim <110741@daum.net>
2025-03-07 19:31:35 +0900

    [Feat] 마이페이지 카드 컴포넌트 1 - 불꽃 아이콘 파일 추가 - 카드 컴포넌트 폴더, 파일 생성 - 카드 컴포넌트 스타일 추가 - 마이페이지 프로필헤더 및 프로필카 컴포넌트화

--- src/pages/my-page/index.tsx
+++ src/pages/my-page/index.tsx
@@ -1,42 +1,33 @@
-import { getPublicImage } from '@/utils/getPublic';
 import './style.css';
 import { useState } from 'react';
+import StatCard from '@/components/MyPage/StatCard';
+import ProfileCard from '@/components/MyPage/ProfileCard';
+import ProfileHeader from '@/components/MyPage/ProfileHeader';
 
 function MyPage() {
-  const [isStatistics] = useState(false);
+  const [isStatistics] = useState(true);
   const [isAchievements] = useState(false);
 
   return (
     <div className="my-page drag-prevent">
-      <h1 className="sr-only">마이페이지</h1>
       <header>상단바 공간</header>
       <main>
         <section className="profile-header">
-          <span className="profile-image-frame">
-            <h2 className="sr-only">프로필 이미지</h2>
-          </span>
-          <button className="edit-profile-btn">
-            <img
-              src={getPublicImage('edit-profile.svg')}
-              alt="프로필 수정하기"
-            />
-          </button>
+          <ProfileHeader />
         </section>
-
         <section className="profile-body">
-          <article className="board profile-details-board">
-            <span className="profile-main-hobby-icon-frame">
-              <h2 className="sr-only">대표 취미 아이콘</h2>
-            </span>
-            <ul>
-              <li>닉네임</li>
-              <li>칭호</li>
-              <li>가입일 : 2025년 3월</li>
-            </ul>
-          </article>
+          <ProfileCard />
           <article className="board">
             <h2>통계</h2>
-            {isStatistics ? <p>통계</p> : <p>통계가 없습니다.</p>}
+            {isStatistics ? (
+              <div className="stat-cards-list">
+                <StatCard />
+                <StatCard />
+                <StatCard />
+              </div>
+            ) : (
+              <p>통계가 없습니다.</p>
+            )}
           </article>
           <article className="board">
             <h2>업적</h2>
