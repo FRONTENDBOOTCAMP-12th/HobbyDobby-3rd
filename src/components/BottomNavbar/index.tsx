2ff37c323c775159672d52e618658c9cc8594394
somin2352 <somin030502@gmail.com>
2025-03-07 22:01:58 +0900

    [Modify] Bottom Navbar
    - route 경로 수정
    - css 수정
    
    resolves #39

--- src/components/BottomNavbar/index.tsx
+++ src/components/BottomNavbar/index.tsx
@@ -8,7 +8,7 @@ import MypageIcon from '/assets/mypage.svg';
 function BottomNavbar() {
   return (
     <nav className="bottom-navbar" aria-label="하단 네비게이션바">
-      <Navlink to="/" image={HomeIcon} menu="홈" />
+      <Navlink to="/home" image={HomeIcon} menu="홈" />
       <Navlink to="/reader-board" image={ReaderBoardIcon} menu="리더보드" />
       <Navlink to="/store" image={StoreIcon} menu="스토어" />
       <Navlink to="/mypage" image={MypageIcon} menu="내정보" />
