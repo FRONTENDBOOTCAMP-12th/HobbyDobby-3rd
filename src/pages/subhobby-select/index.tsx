fbf3681d5b867a783733214b3dd3535c4fed28b4
somin2352 <somin030502@gmail.com>
2025-03-07 14:04:39 +0900

    [Feat] SubHobby Select Page
    - sub_hobby supabase 연결

--- src/pages/subhobby-select/index.tsx
+++ src/pages/subhobby-select/index.tsx
@@ -2,27 +2,47 @@ import './style.css';
 import LeftArrow from '/assets/left-arrow.svg';
 import Logo from '/assets/large-logo.svg';
 import SubHobbySelectCard from '@/components/SubHobbySelect/SubHobbySelectCard';
+import { useEffect, useState } from 'react';
+import { supabase } from '@/lib/supabase-client';
+import { getHobbyIcon } from '@/utils/getHobbyIcon';
+import { Link, useParams } from 'react-router';
 
-const exampleData = [
-  {
-    id: 1,
-    name: '문학',
-    info: '세부 장르에 대한 간단한 소개문, 세부 장르 챌린지에 대한 간략한 정보를 담는 텍스트 공간',
-  },
-  {
-    id: 2,
-    name: '비문학',
-    info: '세부 장르에 대한 간단한 소개문, 세부 장르 챌린지에 대한 간략한 정보를 담는 텍스트 공간22222',
-  },
-];
+interface subHobbiesProps {
+  hobby_id: string | null;
+  id: string;
+  info: string;
+  name: string;
+}
 
 function SubHobbySelectPage() {
+  const [subHobbies, setSubHobbies] = useState<subHobbiesProps[] | null>([]);
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await supabase.from('sub_hobby').select('*');
+        setSubHobbies(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData().catch((error) => {
+      console.log('Error fetching subhobbies:', error);
+    });
+  }, []);
+
   return (
     <div className="subhobby-select">
       <header className="subhobby-select__header">
-        <button className="subhobby-select__button" aria-label="뒤로 가기">
+        <Link
+          to="/select-hobby"
+          className="subhobby-select__button"
+          aria-label="뒤로 가기"
+        >
           <img src={LeftArrow} alt="뒤로 가기" aria-hidden="true" />
-        </button>
+        </Link>
         <h1 className="subhobby-select__logo">
           <span className="sr-only">Hobby Dobby</span>
           <img src={Logo} alt="Hobby Dobby" aria-hidden="true" />
@@ -30,11 +50,11 @@ function SubHobbySelectPage() {
       </header>
       <div className="subhobby-select__content">
         <div className="subhobby-select__selected">
-          <img src="/assets/book.svg" alt="독서" />
-          <span>독서</span>
+          <img src={getHobbyIcon(params.name!)} alt={params.name} />
+          <span>{params.name}</span>
         </div>
       </div>
-      {exampleData.map((data) => (
+      {subHobbies?.map((data) => (
         <SubHobbySelectCard key={data.id} name={data.name} info={data.info} />
       ))}
     </div>
