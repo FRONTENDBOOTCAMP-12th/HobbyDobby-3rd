ea784fe7471db443c82ec408637aec59c783231f
kongsh <ksh080838@gmail.com>
2025-03-07 11:06:13 +0900

    [Design] UI 중앙 정렬로 변경

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,14 @@ import { ErrorBoundary } from 'react-error-boundary';
 import PrintError from '@/components/ErrorBoundary';
 // import LoginPage from '@/pages/login';
 // import LandingPage from '@/pages/landing-page';
-import HobbySelectPage from '@/pages/hobby-select';
+// import HobbySelectPage from '@/pages/hobby-select';
 
 function App() {
   return (
     <ErrorBoundary FallbackComponent={PrintError}>
       {/* <LoginPage /> */}
       {/* <LandingPage /> */}
-      <HobbySelectPage />
+      {/* <HobbySelectPage /> */}
     </ErrorBoundary>
   );
 }
