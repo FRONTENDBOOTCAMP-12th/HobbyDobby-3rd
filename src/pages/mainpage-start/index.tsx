d2754643dc03cb1419caa1bd07f732f6ab0e017a
lyb9030 <lyb_9030@naver.com>
2025-03-11 14:15:09 +0900

    [Design] 챌린지 시작 페이지 디자인 수정 - 글씨 왼쪽 정렬 - 버튼 가운데 정렬

--- src/pages/mainpage-start/index.tsx
+++ src/pages/mainpage-start/index.tsx
@@ -1,11 +1,13 @@
 import './style.css';
 import TextForm from '@/components/Form/TextForm';
+import ProgressBar from '@/components/ProgressBar';
 
 function MainpageStart() {
   return (
     <main className="Challengstart">
       <h1 className="sr-only">Hobby Dobby</h1>
       <header></header>
+      <ProgressBar value={30} max={100} />
       <p className="ChallengeName">챌린지의 이름을 정해주세요.</p>
       <form>
         <TextForm />
