4c3b04d44c8daf5d5f7057d48edb57d04e049f4c
kongsh <ksh080838@gmail.com>
2025-03-11 16:27:58 +0900

    [Feat] 챌린지 중/아닌 경우 조건부 렌더링
    - 머지를 위한 중간 커밋
    - 챌린지 중인 경우의 카드는 완성
    - 챌린지를 시작하기 위한 버튼을 가지고 있는 카드 개발 중

--- src/components/MainComponents/MainCard.tsx
+++ src/components/MainComponents/MainCard.tsx
@@ -1,15 +1,28 @@
+import { Link } from 'react-router';
 import './main-card.css';
 
 interface MainCardProps {
-  challengeName: string;
-  section: number;
+  subHobby?: string;
+  challengeName?: string;
+  section?: number;
+  onChallenge?: boolean;
 }
 
-function MainCard({ challengeName, section }: MainCardProps) {
-  return (
+function MainCard({
+  challengeName,
+  section,
+  onChallenge = true,
+  subHobby,
+}: MainCardProps) {
+  return onChallenge ? (
     <section className="main-card">
       <h3>{challengeName}</h3>
       <h4>섹션 {section}</h4>
+    </section>
+  ) : (
+    <section className="main-card">
+      <p>챌린지를 생성해주세요.🥰</p>
+      <Link to={`/select-hobby/${subHobby}`}>새 챌린지 생성</Link>
     </section>
   );
 }
