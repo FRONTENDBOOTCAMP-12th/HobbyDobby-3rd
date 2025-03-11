c1bd2ffa0ccf8148d38d5bbd25d4f9c180cd4675
lyb9030 <lyb_9030@naver.com>
2025-03-11 15:34:37 +0900

    [Modify] 메인페이지 첼린지 시작 페이지 수정 - 필요없는 잔여 코드 삭제 - 단위 변경 px -> rem - 색상 코드 변경 (# -> var)

--- src/pages/mainpage-start/index.tsx
+++ src/pages/mainpage-start/index.tsx
@@ -6,20 +6,15 @@ function MainpageStart() {
   return (
     <main className="Challengstart">
       <h1 className="sr-only">Hobby Dobby</h1>
-      <header></header>
       <ProgressBar value={30} max={100} />
       <p className="ChallengeName">챌린지의 이름을 정해주세요.</p>
-      <form>
-        <TextForm />
-      </form>
+      <TextForm />
       <p className="tip">Tip. 나중에 변경할 수 있어요.</p>
 
       {/* 버튼 부분 */}
-      <div>
-        <button type="button" className="decide">
-          정했어요
-        </button>
-      </div>
+      <button type="button" className="decide">
+        정했어요
+      </button>
     </main>
   );
 }
