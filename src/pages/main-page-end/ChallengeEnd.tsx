176da4458ddb92f12f00c44cab773f8509dfd720
lyb9030 <lyb_9030@naver.com>
2025-03-12 15:47:36 +0900

    [Feat] 챌린지 엔딩페이지 -1 구현 - 챌린지 완주시 나오는 첫 페이지 구현

---
+++ src/pages/main-page-end/ChallengeEnd.tsx
@@ -0,0 +1,14 @@
+import './ChallengeEnd.css';
+
+function ChallengeEnd() {
+  return (
+    <main className="Challengend">
+      <h1 className="sr-only">Hobby Dobby</h1>
+      <img src="/assets/book.svg" alt="" className="bookmark" />
+      <p className="challengname"> 25년 첫 독서 챌린지</p>
+      <p className="theend">완주!</p>
+    </main>
+  );
+}
+
+export default ChallengeEnd;
