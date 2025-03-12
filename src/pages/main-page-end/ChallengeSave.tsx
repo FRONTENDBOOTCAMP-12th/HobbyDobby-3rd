75b8ecdd76f78e8e1934b28130527a48cf25a688
lyb9030 <lyb_9030@naver.com>
2025-03-12 13:38:14 +0900

    [Feat] 챌린지 엔딩페이지-3 구현 - 챌린지 리스트저장하기 부분 구현

---
+++ src/pages/main-page-end/ChallengeSave.tsx
@@ -0,0 +1,16 @@
+import './ChallengeSave.css';
+
+function ChallengeSave() {
+  return (
+    <main className="Challengsave">
+      <h1 className="sr-only">Hobby Dobby</h1>
+      <p>
+        <span className="challeng">25년 첫 독서 챌린지</span>
+        <span className="challengother">를</span>
+      </p>
+      <p className="listsave">를 리스트에 저장할게요....</p>
+    </main>
+  );
+}
+
+export default ChallengeSave;
