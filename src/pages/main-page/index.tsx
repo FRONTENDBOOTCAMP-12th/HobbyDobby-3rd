7a18c242e18ada9e252e43d8ff1b8d2de88fd3bf
kongsh <ksh080838@gmail.com>
2025-03-13 09:29:17 +0900

    [Modify] MainPage에 Title 추가
    - h2 값도 Title에 맞게 수정

--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,16 +1,20 @@
 import './style.css';
 import MainCard from '@/components/MainComponents/MainCard';
 import UnitButton from '@/components/MainComponents/UnitButton';
+import Title from '@/layouts/title';
 
 function MainPage() {
   return (
-    <main className="main-page">
-      <h2 className="sr-only">챌린지 메인 페이지</h2>
-      <MainCard challengeName={null} section={1} hobby="독서" />
-      <UnitButton unitTitle="유닛 1-1의 타이틀" buttonState="complete" />
-      <UnitButton unitTitle="유닛 1-2의 타이틀" buttonState="now" />
-      <UnitButton unitTitle="유닛 1-3의 타이틀" />
-    </main>
+    <>
+      <Title>메인 페이지</Title>
+      <main className="main-page">
+        <h2 className="sr-only">메인 페이지</h2>
+        <MainCard challengeName={null} section={1} hobby="독서" />
+        <UnitButton unitTitle="유닛 1-1의 타이틀" buttonState="complete" />
+        <UnitButton unitTitle="유닛 1-2의 타이틀" buttonState="now" />
+        <UnitButton unitTitle="유닛 1-3의 타이틀" />
+      </main>
+    </>
   );
 }
 
