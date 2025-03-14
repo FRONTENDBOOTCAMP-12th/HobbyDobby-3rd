171941a111d43c3ad83d7f27c6fe9806f8a3dda5
lyb9030 <lyb_9030@naver.com>
2025-03-14 14:24:56 +0900

    [Feat] 엔딩페이지 -4 구현 - 챌린지 완료시 저장 완료 문구 구현

---
+++ src/pages/main-page-end/ChallengeExit.tsx
@@ -0,0 +1,12 @@
+import './ChallengeExit.css';
+
+function ChallengeExit() {
+  return (
+    <main className="Challengeexit">
+      <h1 className="sr-only">Hobby Dobby</h1>
+      <p className="Challengsavesucceed">저장 완료 !</p>
+    </main>
+  );
+}
+
+export default ChallengeExit;
