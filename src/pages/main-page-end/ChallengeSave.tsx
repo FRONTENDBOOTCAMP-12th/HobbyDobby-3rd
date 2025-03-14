a0ec44948bec1a24ff6c1d28ba423b61348e565f
lyb9030 <lyb_9030@naver.com>
2025-03-14 17:31:16 +0900

    [Modify] save 파일 오타 수정

--- src/pages/main-page-end/ChallengeSave.tsx
+++ src/pages/main-page-end/ChallengeSave.tsx
@@ -2,11 +2,11 @@ import './ChallengeSave.css';
 
 function ChallengeSave() {
   return (
-    <main className="Challeng-save">
+    <main className="Challenge-save">
       <h1 className="sr-only">Hobby Dobby</h1>
       <p>
-        <span className="challeng">25년 첫 독서 챌린지</span>
-        <span className="challengother">를</span>
+        <span className="challengetarget">25년 첫 독서 챌린지</span>
+        <span className="challengeother">를</span>
       </p>
       <p className="listsave">를 리스트에 저장할게요....</p>
     </main>
