92307806c9c92c99bdac2db81d0e8994e08dc6da
lyb9030 <lyb_9030@naver.com>
2025-03-17 00:20:48 +0900

    [Modify] Classnaeme BEM 패턴으로 변경

--- src/components/MainpageEnd/ChallengeFinsh.tsx
+++ src/components/MainpageEnd/ChallengeFinsh.tsx
@@ -4,9 +4,9 @@ function ChallengeFinsh() {
   return (
     <main className="Challenge-end">
       <h1 className="sr-only">Hobby Dobby</h1>
-      <img src="/assets/book.svg" alt="" className="bookmark" />
-      <p className="challengname"> 25년 첫 독서 챌린지</p>
-      <p className="theend">완주!</p>
+      <img src="/assets/book.svg" alt="" className="Challenge-end_bookmark" />
+      <p className="Challenge-end_challeng-name"> 25년 첫 독서 챌린지</p>
+      <p className="Challenge-end_the-end">완주!</p>
     </main>
   );
 }
--- src/components/MainpageEnd/ChallengeSave.tsx
+++ src/components/MainpageEnd/ChallengeSave.tsx
@@ -5,10 +5,12 @@ function ChallengeSave() {
     <main className="Challenge-save">
       <h1 className="sr-only">Hobby Dobby</h1>
       <p>
-        <span className="challengetarget">25년 첫 독서 챌린지</span>
-        <span className="challengeother">를</span>
+        <span className="Challenge-save_challenge-target">
+          25년 첫 독서 챌린지
+        </span>
+        <span className="Challenge-save_challenge-other">를</span>
       </p>
-      <p className="listsave">를 리스트에 저장할게요....</p>
+      <p className="Challenge-save_listsave">를 리스트에 저장할게요....</p>
     </main>
   );
 }
