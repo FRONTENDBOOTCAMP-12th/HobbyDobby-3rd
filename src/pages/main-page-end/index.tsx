4e3afcd73af646e3020c14de8cfc9b9ca9c662d0
lyb9030 <lyb_9030@naver.com>
2025-03-17 00:26:25 +0900

    [Comment] MainPageEnd 진행 순서에 따른 주석 작성

---
+++ src/pages/main-page-end/index.tsx
@@ -0,0 +1,16 @@
+import './style.css';
+import ChallengeFinsh from '@/components/MainpageEnd/ChallengeFinsh';
+
+function MainPageEnd() {
+  return (
+    <main className="MainPageend">
+      <h1 className="sr-only">Hobby Dobby</h1>
+      <ChallengeFinsh />
+      {/* 컴포넌트의 순서는 finsh -> review -> save -> exit 의 순서로 진행*/}
+      {/* review 컴포넌트에서 하단에 저장하기 버튼을 누르면 save 컴포넌트로 이동 */}
+      {/* save 컴포넌트에서 review 페이지창에서 입력한 내용이 db에 업로드가 완료되면 exit 컴포넌트로 이동 */}
+    </main>
+  );
+}
+
+export default MainPageEnd;
