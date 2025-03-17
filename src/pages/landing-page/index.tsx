c16a02c9c105bcfadea7f21159c2e4eefa4fd6fc
lyb9030 <lyb_9030@naver.com>
2025-03-17 15:08:57 +0900

    [Refactor] 중간점검 개선 요청 사항 반영

--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -4,26 +4,17 @@ import './style.css';
 function LandingPage() {
   return (
     <main className="landing">
-      <h1 className="sr-only">Hobby Dobby</h1>
       <header>
-        <div>
-          {/* 로고 */}
-          <div>
-            <img
-              src="/assets/logo.svg"
-              alt="Hobby Dobby 로고"
-              className="logo"
-            />
-            {/* 로고 텍스트 이미지 */}
-            <div className="logo-text">
-              <img src="/assets/hobby.svg" alt="" className="hobby-image" />
-              <img src="/assets/dobby.svg" alt="" className="dobby-image" />
-            </div>
+        <h1 className="sr-only">Hobby Dobby</h1>
+        <figure>
+          <img src="/assets/logo.svg" alt="" className="logo" />
+          <div className="logo-text">
+            <img src="/assets/hobby.svg" alt="" className="hobby-image" />
+            <img src="/assets/dobby.svg" alt="" className="dobby-image" />
           </div>
-        </div>
+        </figure>
       </header>
 
-      {/* 버튼 */}
       <div>
         <Link to="/login" className="start">
           시작하기
