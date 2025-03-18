1420b20cafd0938442429e1ea8e3d71f5d3f17c1
kongsh <ksh080838@gmail.com>
2025-03-18 11:19:34 +0900

    [Feat] 파일 업로드 컴포넌트
    - 파일 미리보기 기능 구현

---
+++ src/components/UnitPage/AnswerTypes/UploadFile.tsx
@@ -0,0 +1,62 @@
+import './upload-file.css';
+import { useRef, useState } from 'react';
+
+interface UploadFileProps {
+  name: string;
+}
+
+interface FilePreview {
+  file: File;
+  preview: string;
+}
+
+function UploadFile({ name }: UploadFileProps) {
+  const [file, setFile] = useState<FilePreview | null>(null);
+  const inputRef = useRef(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const selectedFile = e.target.files[0];
+
+      const nextFile = {
+        file: selectedFile,
+        preview: URL.createObjectURL(selectedFile),
+      };
+
+      setFile(nextFile);
+    }
+  };
+
+  const removeFile = () => {
+    setFile(null);
+
+    (inputRef.current! as HTMLInputElement).value = '';
+  };
+
+  return (
+    <div className="upload-file">
+      {file ? (
+        <img src={file?.preview} alt="preview" />
+      ) : (
+        <div className="upload-file__preview"></div>
+      )}
+      <div className="upload-file__input-wrapper">
+        <label>
+          <span role="button">파일 업로드</span>
+          <input
+            type="file"
+            name={name}
+            onChange={handleFileChange}
+            ref={inputRef}
+          />
+        </label>
+
+        <button type="button" onClick={removeFile}>
+          삭제
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default UploadFile;
