8daf24173a7b55f1ee5fccdaee04418e7401bad9
lyb9030 <lyb_9030@naver.com>
2025-03-19 10:37:39 +0900

    [Feat] 스토어 아이템 컴포넌트 개발

---
+++ src/components/StoreItem/storeitem.tsx
@@ -0,0 +1,16 @@
+import './store-item.css';
+
+function StoreItem() {
+  return (
+    <main className="asdfasdf">
+      <h1 className="sr-only">Hobby Dobby</h1>
+      <p>아이템 이미지</p>
+      <p>가격</p>
+      <p>아이템 명</p>
+      <p>아이템 설명</p>
+      <p>구입하기 버튼</p>
+    </main>
+  );
+}
+
+export default StoreItem;
