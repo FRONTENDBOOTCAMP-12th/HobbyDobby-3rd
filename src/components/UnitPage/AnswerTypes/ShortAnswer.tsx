da6ab13d0ca6ba5f8878a78acd36dd8b9814aa26
kongsh <ksh080838@gmail.com>
2025-03-20 11:41:31 +0900

    [Modify] 단답형 컴포넌트 label 추가

--- src/components/UnitPage/AnswerTypes/ShortAnswer.tsx
+++ src/components/UnitPage/AnswerTypes/ShortAnswer.tsx
@@ -1,15 +1,24 @@
 import './short-answer.css';
+import { useId } from 'react';
 
 type ShortAnswerProps = React.ComponentProps<'input'>;
 
 function ShortAnswer({ ...restProps }: ShortAnswerProps) {
+  const shortAnswerId = useId();
+
   return (
-    <input
-      className="short-answer"
-      type="text"
-      placeholder="문자를 입력해주세요"
-      {...restProps}
-    />
+    <>
+      <label htmlFor={shortAnswerId} className="sr-only">
+        단답형 답란
+      </label>
+      <input
+        id={shortAnswerId}
+        className="short-answer"
+        type="text"
+        placeholder="문자를 입력해주세요"
+        {...restProps}
+      />
+    </>
   );
 }
 
