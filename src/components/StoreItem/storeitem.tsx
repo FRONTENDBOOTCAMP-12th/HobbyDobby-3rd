16a49375677c0b152a35dd57224f5cf8a5ed8ea1
lyb9030 <lyb_9030@naver.com>
2025-03-21 03:43:47 +0900

    [Design] Css 파일 수정 - 아이템 DB 사이즈에 맞추어 변경 - ClassName 수정 - 단위 변경 px -> rem

--- src/components/StoreItem/storeitem.tsx
+++ src/components/StoreItem/storeitem.tsx
@@ -36,24 +36,34 @@ function StoreItem() {
   }, []);
 
   return (
-    <div className="store-item">
-      {loading && <p className="loading-text">로딩 중...</p>}
-      {error && <p className="error-text">{error}</p>}
-      {!loading && items.length === 0 && (
-        <p className="empty-text">아이템이 없습니다.</p>
-      )}
+    <div className="store-item__container">
+      {loading && <p>로딩 중...</p>}
+      {error && <p>{error}</p>}
+      {!loading && items.length === 0 && <p>아이템이 없습니다.</p>}
 
       {/* 아이템 목록 */}
       {items.map((item) => (
-        <div key={item.id} className="store-item__item">
-          <img src={item.image} alt={item.name} className="item-image" />
-          <div className="item-info">
-            <h3 className="item-name">{item.name}</h3>
-            <p className="item-description">{item.description}</p>
-            <div className="item-footer">
-              <span className="item-price">💎 {item.price}</span>
-              <button className="buy-button">구입하기</button>
+        <div key={item.id} className="store-item__itemdb">
+          <div className="store-item__left">
+            <img
+              src={item.image}
+              alt={item.name}
+              className="store-item__image"
+            />
+            <br></br>
+            <div className="store-item__gemp-rice">
+              <img src="/assets/gem.svg" alt="" className="store-item__gem" />
+              <span className="store-item__price"> {item.price}</span>
             </div>
+          </div>
+          <div className="store-item__center">
+            <p className="store-item__name">{item.name}</p>
+            <p className="store-item__explanation">
+              아이템으로 프로필을 멋지게 꾸며보세요!
+            </p>
+          </div>
+          <div className="store-item__right">
+            <button className="store-item__buy-button">구입하기</button>
           </div>
         </div>
       ))}
