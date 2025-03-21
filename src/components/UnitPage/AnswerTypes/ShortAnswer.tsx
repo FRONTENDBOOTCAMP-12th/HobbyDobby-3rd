4a9351d9183bf37ed52fca507c454aeb9d047dd4
kongsh <ksh080838@gmail.com>
2025-03-21 22:40:56 +0900

    [Feat] 문항 컴포넌트 디바운스 및 인풋 이벤트 추가
    - fill을 제외한 문항 컴포넌트들에 디바운스, 인풋 이벤트 추가

--- src/components/UnitPage/AnswerTypes/ShortAnswer.tsx
+++ src/components/UnitPage/AnswerTypes/ShortAnswer.tsx
@@ -1,13 +1,21 @@
+import { debounce } from '@/utils/debounce';
 import './short-answer.css';
 import { useId } from 'react';
 
-type ShortAnswerProps = React.ComponentProps<'input'>;
+type ShortAnswerProps = React.ComponentProps<'input'> & {
+  setNextAnswers: (value: string[], type: string) => void;
+};
 
-function ShortAnswer({ ...restProps }: ShortAnswerProps) {
+function ShortAnswer({ key, setNextAnswers, ...restProps }: ShortAnswerProps) {
   const shortAnswerId = useId();
 
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setNextAnswers([e.target.value], 'short_descript');
+
+  const debouncedInput = debounce(handleInput, 300);
+
   return (
-    <>
+    <div className="short-answer__wrapper" key={key}>
       <label htmlFor={shortAnswerId} className="sr-only">
         단답형 답란
       </label>
@@ -16,9 +24,10 @@ function ShortAnswer({ ...restProps }: ShortAnswerProps) {
         className="short-answer"
         type="text"
         placeholder="문자를 입력해주세요"
+        onChange={(e) => debouncedInput(e)}
         {...restProps}
       />
-    </>
+    </div>
   );
 }
 
